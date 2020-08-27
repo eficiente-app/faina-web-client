4cc4aeaeec76914c1c83298287bf90ca4ac2ac7a
Eder Ferraz Caciano <eder.ferraz.caciano>
2020-08-26 22:18:42 -0400

    Escopo inicial de rotas, layout principal e menus

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,29 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/eqeqeq': 'error',
+    'vue/component-tags-order': 'warn',
+    'vue/script-indent': 'error'
+  },
+
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/standard'
+  ],
+
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/standard'
+  ]
 }
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pwa: {
+    name: 'Faina'
+  }
 }
