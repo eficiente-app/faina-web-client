500df730645181ee5578b89fc2ca032cc51f00ce
Eder Ferraz Caciano <70242836+eder-ferraz-caciano@users.noreply.github.com>
2021-01-19 23:20:47 -0400

    Finalização do componente modal

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -2,38 +2,59 @@
   <div class="text-center">
     <v-dialog
       :value="value"
-      :width="this.width"
+      :width="options && options.width || optionsDefalt.width"
       persistent
       @input="$emit('input', value)"
     >
       <v-card>
-        <v-card-title class="headline grey lighten-2">
-          {{ title }}
+        <v-card-title
+          class="headline secondary white--text py-2 px-2"
+          dark
+        >
+          <v-btn
+            color="white"
+            icon
+            left
+            size="45"
+            @click="$emit('close')"
+          >
+            <v-icon class="pa-0 ma-0">
+              mdi-close
+            </v-icon>
+          </v-btn>
+          {{ options && options.title ? options.title : optionsDefalt.title }}
         </v-card-title>
 
-        <v-card-text>
-          <slot name="listagem" />
+        <v-card-text class="pa-0">
+          <slot />
         </v-card-text>
 
         <v-divider></v-divider>
 
-        <v-card-actions>
+        <v-card-actions v-if="options && options.actions && options.actions.length">
           <v-spacer></v-spacer>
           <v-btn
-            color="primary"
-            outlined
-            @click="$emit('salvar')"
+            v-for="btn in options.actions"
+            :color="btn.color"
+            :key="btn.title"
+            :outlined="btn.outlined"
+            @click="$emit(`${btn.emit}`)"
           >
-            <v-icon left>mdi-content-save</v-icon>
-            Salvar
+            <v-icon left>{{btn.icon}}</v-icon>
+            {{ btn.title }}
           </v-btn>
+        </v-card-actions>
+        <v-card-actions v-else>
+          <v-spacer></v-spacer>
           <v-btn
-            color="error"
-            outlined
-            @click="$emit('input', false)"
+            v-for="def in optionsDefalt.actions"
+            :color="def.color"
+            :key="def.title"
+            :outlined="def.outlined"
+            @click="$emit(`${def.emit}`)"
           >
-            <v-icon left>mdi-cancel</v-icon>
-            Cancel
+            <v-icon left>{{def.icon}}</v-icon>
+            {{ def.title }}
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -46,25 +67,44 @@ export default {
   name: 'DialogComponent',
 
   data: () => ({
-    //
+    /**
+     * @author Eder Ferraz Caciano
+     * @summary optionsDefalt
+     * @description Objeto padrão para carregamento do componente modal. Cada opção pode
+     * ser passado de forma dinamica dentro da prop options.
+     * Os valores aceitos na prop options são os mesmos da variavel optionsDefault.
+     * Caso seja passado valor em options será renderizado, caso não o valor padrão será carregado.
+     */
+    optionsDefalt: {
+      title: 'Form Title',
+      width: 600,
+      actions: [
+        {
+          title: 'Save',
+          color: 'primary',
+          icon: 'mdi-content-save',
+          emit: 'salvar',
+          outlined: false
+        },
+        {
+          title: 'Cancel',
+          color: 'error',
+          icon: 'mdi-cancel',
+          emit: 'cancel',
+          outlined: false
+        }
+      ]
+    }
   }),
 
   props: {
-    title: {
-      default: 'Form Title',
-      type: String
+    options: {
+      default: () => {}
     },
     value: {
       required: true,
       type: Boolean
-    },
-    width: {
-      default: 700
     }
   }
 }
 </script>
-
-<style>
-
-</style>
